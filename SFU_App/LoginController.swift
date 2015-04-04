8bb2915cf7144b52c66276a24074f2c9616d1c81
Nethaniel Yuen <nyyuen@sc-2300-49.labs.surrey.sfu.ca>
2015-04-04 16:12:31 -0700

    Fixed textfields for login so that enter will let you move to next field and login and stuff

--- SFU_App/LoginController.swift
+++ SFU_App/LoginController.swift
@@ -11,7 +11,7 @@ import Foundation
 // View controller responsible for SFU authentication for both SIS system and CAS, serves to maintain login state for canvas, connect and SIS//
 var USERID = ""
 let defaults = NSUserDefaults.standardUserDefaults()
-class LoginController: UIViewController,UIWebViewDelegate,ENSideMenuDelegate {
+class LoginController: UIViewController,UIWebViewDelegate,ENSideMenuDelegate,UITextFieldDelegate {
     // Reference to login and password fields//
     @IBOutlet weak var login: UITextField!
     @IBOutlet weak var password: UITextField!
@@ -71,6 +71,10 @@ class LoginController: UIViewController,UIWebViewDelegate,ENSideMenuDelegate {
     override func viewDidLoad() {
         
         super.viewDidLoad()
+        
+        // Delegates for textfield
+        login.delegate = self
+        password.delegate = self
         
         // Check for internet connection
         self.outputError()
@@ -139,6 +143,19 @@ class LoginController: UIViewController,UIWebViewDelegate,ENSideMenuDelegate {
         
     }
     
+    // Set textfield so that after pressing Next in login textfield, it will move to password textfield
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        if(login.isFirstResponder()){
+            login.resignFirstResponder()
+            password.becomeFirstResponder()
+        }
+        
+        else if(password.isFirstResponder()){
+            password.resignFirstResponder()
+            loginAction(loginButton)
+        }
+        return true
+    }
     
     // Do any additional setup after loading the view.
     
